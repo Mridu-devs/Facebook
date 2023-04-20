a969b0217555aa2a9219ebc601cbb6fc0a285c0a
Mridu-devs <mridu.devs@gmail.com>
2023-04-20 14:17:36 +0530

    created Posts & Postcard component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,4 +1,31 @@
+import PostCard from "../postcard/PostCard";
 import "./posts.scss";
 export default function Posts() {
-  return <div className="posts">Posts</div>;
+  //Temporary
+  const posts = [
+    {
+      id: 1,
+      name: "John Doe",
+      userId: 1,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    },
+    {
+      id: 2,
+      name: "Jenny Doe",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+    },
+  ];
+  return (
+    <div className="posts">
+      {posts.map((post) => (
+        <PostCard post={post} key={post.id} />
+      ))}
+    </div>
+  );
 }
