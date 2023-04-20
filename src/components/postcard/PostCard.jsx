73dfd7fb3ddcb584b658b357b607c3c59554f963
Mridu-devs <mridu.devs@gmail.com>
2023-04-20 14:49:53 +0530

    added Comment component

--- src/components/postcard/PostCard.jsx
+++ src/components/postcard/PostCard.jsx
@@ -5,8 +5,13 @@ import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
+import Comments from "../comments/Comments";
+import { useState } from "react";
 
 function PostCard({ post }) {
+  const [commentOpen, setCommentOpen] = useState(false);
+
+  //Temporary
   const liked = false;
   return (
     <div className="postcard">
@@ -35,7 +40,7 @@ function PostCard({ post }) {
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
             12 Likes
           </div>
-          <div className="item">
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
             12 Comments
           </div>
@@ -43,6 +48,7 @@ function PostCard({ post }) {
             <ShareOutlinedIcon /> Share
           </div>
         </div>
+        {commentOpen && <Comments />}
       </div>
     </div>
   );
