81e919a7ee70174c7d006427ecf9663de22b6aed
Mridu-devs <mridu.devs@gmail.com>
2023-04-23 13:56:02 +0530

    made the website device responsive

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Posts from "../../components/posts/Posts";
+import Share from "../../components/share/Share";
 import Stories from "../../components/stories/Stories";
 import "./home.scss";
 
@@ -8,6 +9,7 @@ const Home = () => {
       <div style={{ width: "90%" }}>
         <Stories />
       </div>
+      <Share/>
       <Posts />
     </div>
   );
